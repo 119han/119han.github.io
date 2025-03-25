de355457e1bac2c804a4087a58b35fc5e8b9eba0
119han <2557432690@qq.com>
2025-03-25 11:03:28 +0800

    语音补全

--- wangxiaotao/sy.js
+++ wangxiaotao/sy.js
@@ -1,38 +1,53 @@
 document.addEventListener('DOMContentLoaded', function() {
   var audioPlayer = document.getElementById('audioPlayer');
 
+  document.getElementById('word0').addEventListener('click', function() {
+    audioPlayer.src = 'wxt/zbcf.mp3'; // 替换为你的音频文件路径
+    audioPlayer.play();
+  });
+
   document.getElementById('word1').addEventListener('click', function() {
       audioPlayer.src = 'wxt/kuyashi.mp3'; // 替换为你的音频文件路径
       audioPlayer.play();
   });
 
   document.getElementById('word2').addEventListener('click', function() {
-      audioPlayer.src = 'wxt/omg.mp3'; // 替换为你的音频文件路径
-      audioPlayer.play();
-  });
-
-  document.getElementById('word3').addEventListener('click', function() {
       audioPlayer.src = 'wxt/ngg.mp3'; // 替换为你的音频文件路径
       audioPlayer.play();
   });
 
-  document.getElementById('word4').addEventListener('click', function() {
+  document.getElementById('word3').addEventListener('click', function() {
     audioPlayer.src = 'wxt/game.mp3'; // 替换为你的音频文件路径
     audioPlayer.play();
   });
 
-  document.getElementById('word5').addEventListener('click', function() {
+  document.getElementById('word4').addEventListener('click', function() {
     audioPlayer.src = 'wxt/KU.mp3'; // 替换为你的音频文件路径
     audioPlayer.play();
-});
+  });
+
+document.getElementById('word5').addEventListener('click', function() {
+  audioPlayer.src = 'wxt/wbhl.mp3'; // 替换为你的音频文件路径
+  audioPlayer.play();
+  });
 
 document.getElementById('word6').addEventListener('click', function() {
-  audioPlayer.src = 'wxt/wbhl.mp3'; // 替换为你的音频文件路径
+  audioPlayer.src = 'wxt/hqjpt.mp3'; // 替换为你的音频文件路径
   audioPlayer.play();
 });
 
 document.getElementById('word7').addEventListener('click', function() {
-    audioPlayer.src = 'wxt/zbcf.mp3'; // 替换为你的音频文件路径
-    audioPlayer.play();
-  });
+  audioPlayer.src = 'wxt/lgwxt.mp3'; // 替换为你的音频文件路径
+  audioPlayer.play();
+});
+
+document.getElementById('word8').addEventListener('click', function() {
+  audioPlayer.src = 'wxt/szwxt.mp3'; // 替换为你的音频文件路径
+  audioPlayer.play();
+});
+
+document.getElementById('word9').addEventListener('click', function() {
+  audioPlayer.src = 'wxt/omg.mp3'; // 替换为你的音频文件路径
+  audioPlayer.play();
+});
 });
