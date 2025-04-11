668d33a9f8b58b41b444fb60cd37a918e550f136
119han <2557432690@qq.com>
2025-04-11 11:50:58 +0800

    主题更新

--- javaS/indeex1.js
+++ javaS/indeex1.js
@@ -1,3 +1,23 @@
 function bili() {
     window.location.replace("https://space.bilibili.com/1458294782?spm_id_from=333.1007.0.0")
 }
+
+function phira() {
+    window.location.replace("/phirapifu.html")
+}
+
+function als() {
+    window.location.replace("/alsbot/lt.html")
+}
+
+function wxt() {
+    window.location.replace("/wangxiaotao/wxt.html")
+}
+
+function xyx() {
+    window.location.replace("/xyx.html")
+}
+
+function A439() {
+    window.location.replace("https://a439-owo.github.io")
+}
