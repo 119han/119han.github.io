8d1b00d0089ddae193700eebe12966b42aaa03bb
119han <2557432690@qq.com>
2025-04-15 15:45:25 +0800

    五子棋

--- javaS/xyx.js
+++ javaS/xyx.js
@@ -1,3 +1,7 @@
+function fh() {
+    window.location.replace("/index1.html")
+}
+
 function fanhu() {
     window.location.replace("/xyx/扫雷.html")
 }
@@ -6,6 +10,6 @@ function tcsa() {
     window.location.replace("/xyx/贪吃蛇.html")
 }
 
-function fh() {
-    window.location.replace("index1.html")
+function wzq() {
+    window.location.replace("/xyx/五子棋.html")
 }
